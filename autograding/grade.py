8223f959d179c612c7f1749e4f860ef83273427c
Steven-Maurice <steven.pro@steven-maurice.com>
2024-02-06 10:42:29 +0100

    added unittest function from __name__

--- autograding/grade.py
+++ autograding/grade.py
@@ -8,4 +8,20 @@ def calculate_grade(score):
     elif score >= 60:
         return 'D'
     else:
-        return 'F'
+        return 'E'
+
+if __name__ == "__main__":
+    # Run the tests
+    import unittest
+    class TestGrade(unittest.TestCase):
+        def test_grade_A(self):
+            self.assertEqual(calculate_grade(95), 'A')
+        def test_grade_B(self):
+            self.assertEqual(calculate_grade(85), 'B')
+        def test_grade_C(self):
+            self.assertEqual(calculate_grade(75), 'C')
+        def test_grade_D(self):
+            self.assertEqual(calculate_grade(65), 'D')
+        def test_grade_E(self):
+            self.assertEqual(calculate_grade(55), 'E')
+    unittest.main()
