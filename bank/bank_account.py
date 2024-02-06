1e932526938a967ebdd89f223d15c0a1de20e3e3
Steven-Maurice <steven.pro@steven-maurice.com>
2024-02-06 07:09:46 +0100

    Ajout de la class bank

---
+++ bank/bank_account.py
@@ -0,0 +1,16 @@
+class BankAccount:
+    def __init__(self, initial_balance=0):
+        self.balance = initial_balance
+
+    def deposit(self, amount):
+        if amount <= 0:
+            raise ValueError("Le montant du dépôt doit être supérieur à zéro.")
+        self.balance += amount
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            raise ValueError("Fonds insuffisants pour effectuer cette opération.")
+        self.balance -= amount
+
+    def get_balance(self):
+        return self.balance
